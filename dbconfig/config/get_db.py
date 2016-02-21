73276a168def407ebcee4a9980a11a85a2b37bca
rv816 <ryan.vass.junk@gmail.com>
2016-02-20 22:09:14 -0500

    abstracted creation of config

--- dbconfig/config/get_db.py
+++ dbconfig/config/get_db.py
@@ -5,12 +5,33 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 
+def register_config():
+    connection = {}
+    try:
+        input = raw_input
+    except NameError:
+        pass
+    
+    connection['profile_name'] = input('Profile Name? ')
+    connection['dbms'] = input('DBMS? ')
+    connection['user'] = input('username? ')
+    connection['password'] = input('Password? ')
+    connection['module'] = input('DB Driver Name? (python package)')
+    connection['host'] = input('Host? ')
+
+    connection['port']= input('Port? ')
+    connection['charset'] = input('Charset? ')
+        
+    with open(str(script_dir) + '/config_{profile_name}.json'.format(profile_name=connection['profile_name']), 'w') as f:
+        json.dump(f, connection)
+
+
 
 class Configurator:
-    def __init__(self, script_dir=script_dir, dbname=''):
-        self.jsonpath = str(script_dir) + '/config.json' 
+    def __init__(self, profile_name = 'default', script_dir=script_dir, dbname=''):
+        self.jsonpath = str(script_dir) + '/config_default.json'
         self.dbname = dbname
-
+        
         with open(self.jsonpath) as config:
             self.CONFIG = json.load(config)
             self.dbms = CONFIG['dbms']
@@ -19,6 +40,8 @@ class Configurator:
             self.password = CONFIG['password']
             self.host = CONFIG['host']
             self.port = CONFIG['port']
+            self.charset = CONFIG['charset']
+
             if self.dbname != '':
                 self.dbname = '/' + self.dbname
             self.url ='{dbms}+{module}://{user}:{password}@{host}:{port}/{dbname}?charset=self.utf8'.format(dbms=self.dbms,module=self.module, user=self.user, password=self.password, host=self.host, port=self.port, dbname=self.dbname)
